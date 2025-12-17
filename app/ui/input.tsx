3062f27df6aed474c7b532144d8746ff80f7468a
Mark W <markwuu@gmail.com>
2025-12-17 14:55:27 -0800

    created reusable input component

---
+++ app/ui/input.tsx
@@ -0,0 +1,39 @@
+export default function Input({
+	id,
+	name,
+	placeholder,
+	value,
+	type,
+	required = true,
+}: {
+	type: string;
+	id: string;
+	name: string;
+	placeholder?: string;
+	value?: string;
+	required?: boolean;
+}) {
+	if (type === 'text') {
+		return (
+			<input
+				type={type}
+				id={id}
+				name={name}
+				placeholder={placeholder}
+				value={value}
+				required={required}
+				className="bg-red-50 text-black w-full px-3 py-3 my-2 border border-gray-300 rounded"
+			/>
+		);
+	} else if (type === 'checkbox') {
+		return (
+			<input
+				type="checkbox"
+				id={id}
+				name={name}
+				value={value}
+				className="h-6 w-6"
+			/>
+		);
+	}
+}
