ee0c38ea448e0acc7bdecb51e06a286e94e874d7
Mark W <markwuu@gmail.com>
2025-12-17 13:33:59 -0800

    created summary component

---
+++ app/cognitive-behavioral-therapy/steps/summary.tsx
@@ -0,0 +1,11 @@
+export default function Summary() {
+	return (
+		<div className="flex flex-col">
+			<h1>Summary</h1>
+			<h2>Automatic Thought:</h2>
+			<h2>Distortions:</h2>
+			<h2>Evidence:</h2>
+			<h2>Reframed Thought:</h2>
+		</div>
+	);
+}
