2b99af5ea3e925224a72c60b36bf2dfd90b32cbd
Mark W <markwuu@gmail.com>
2025-12-17 16:43:03 -0800

    created handlechange function for thought step

--- app/cognitive-behavioral-therapy/steps/thought.tsx
+++ app/cognitive-behavioral-therapy/steps/thought.tsx
@@ -1,16 +1,28 @@
 import Input from '@/app/ui/input';
 import Title from '@/app/ui/title';
+import { FC } from 'react';
 
-export default function Thought() {
+interface ChildProps {
+	updateCbt: (arg0: string) => void;
+}
+
+export const Thought: FC<ChildProps> = ({ updateCbt }) => {
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+		const { value } = event.target;
+		updateCbt(value);
+	};
+
 	return (
 		<div className="flex flex-col">
-			<Title text="Add Your Thought" />
+			<Title text="1. Add Your Thought" />
 			<Input
 				type="text"
 				id="thought"
 				name="thought"
+				// value={thought}
 				placeholder="Enter your thought here"
+				handleChange={(e) => handleChange(e)}
 			/>
 		</div>
 	);
-}
+};
