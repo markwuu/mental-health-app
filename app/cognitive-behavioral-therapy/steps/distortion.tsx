f983c05559fde7d65700528263033bf0ab46f8af
Mark W <markwuu@gmail.com>
2025-12-17 23:43:58 -0800

    added handle change functions to distortion component

--- app/cognitive-behavioral-therapy/steps/distortion.tsx
+++ app/cognitive-behavioral-therapy/steps/distortion.tsx
@@ -1,41 +1,51 @@
 import Input from '@/app/ui/input';
 import Title from '@/app/ui/title';
+import { FC, useContext } from 'react';
+import { CBTContext, distortion } from '../page';
 
-export default function Distortion() {
-	const distortions = [
-		'catastrophizing',
-		'should statements',
-		'magnification',
-		'minimization',
-		'emotional reasoning',
-		'mental filter',
-		'disqualify the positive',
-		'all-or-nothing thinking',
-		'personalization',
-		'blaming',
-		'jumping to conclusions',
-		'mind reading',
-		'fortune telling',
-		'overgeneralization',
-		'labeling',
-	];
+interface ChildProps {
+	updateCbt: (
+		value: { thought: string } | { distortions: distortion[] },
+	) => void;
+}
+
+export const Distortion: FC<ChildProps> = ({ updateCbt }) => {
+	const cbt = useContext(CBTContext);
+
+	const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { value, checked } = event.target;
+		const updatedDistortion = cbt.distortions.map((distortion) => {
+			if (distortion.label === value) {
+				return { label: value, checked: checked };
+			}
+			return distortion;
+		});
+
+		updateCbt({ distortions: updatedDistortion });
+	};
 
 	return (
 		<div className="flex flex-col">
 			<Title text="2. Select a Distortion" />
 			<div className="grid grid-cols-3 gap-2">
-				{distortions.map((distortion) => (
-					<div className="flex flex-row gap-3 py-1" key={distortion}>
-						<Input
-							type="checkbox"
-							id={distortion}
-							name={distortion}
-							value={distortion}
-						/>
-						<label htmlFor={distortion}>{distortion}</label>
-					</div>
-				))}
+				{cbt.distortions.map(
+					(distortion: { label: string; checked: boolean }) => {
+						return (
+							<div className="flex flex-row gap-3 py-1" key={distortion.label}>
+								<Input
+									type="checkbox"
+									id={distortion.label}
+									value={distortion.label}
+									name={distortion.label}
+									checked={distortion.checked}
+									onChange={(e) => handleCheckboxChange(e)}
+								/>
+								<label htmlFor={distortion.label}>{distortion.label}</label>
+							</div>
+						);
+					},
+				)}
 			</div>
 		</div>
 	);
-}
+};
