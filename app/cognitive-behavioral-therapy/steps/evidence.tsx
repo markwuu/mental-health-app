031c8b1bb39457da6fd08d22e467ac45548c2181
Mark W <markwuu@gmail.com>
2025-12-18 00:04:25 -0800

    added logic to handle evidence text change

--- app/cognitive-behavioral-therapy/steps/evidence.tsx
+++ app/cognitive-behavioral-therapy/steps/evidence.tsx
@@ -1,7 +1,21 @@
 import Input from '@/app/ui/input';
 import Title from '@/app/ui/title';
+import { FC, useContext, useState } from 'react';
+import { CBTContext } from '../page';
 
-export default function Evidence() {
+interface ChildProps {
+	updateCbt: (value: { evidence: string }) => void;
+}
+
+export const Evidence: FC<ChildProps> = ({ updateCbt }) => {
+	const cbt = useContext(CBTContext);
+	const [inputValue, setInputValue] = useState(cbt.evidence);
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+		setInputValue(event.target.value);
+		updateCbt({ evidence: event.target.value });
+	};
+
 	return (
 		<div className="flex flex-col">
 			<Title text="3. Is there any evidence that challenges this?" />
@@ -9,8 +23,10 @@ export default function Evidence() {
 				type="text"
 				id="thought"
 				name="thought"
+				value={inputValue}
 				placeholder="Enter your thought here"
+				handleChange={(e) => handleChange(e)}
 			/>
 		</div>
 	);
-}
+};
