cc8b1702cd0718ca5828b505a6857bbce165e829
Mark W <markwuu@gmail.com>
2025-12-18 00:04:45 -0800

    added logic to handle reframed text change

--- app/cognitive-behavioral-therapy/steps/reframed.tsx
+++ app/cognitive-behavioral-therapy/steps/reframed.tsx
@@ -1,7 +1,21 @@
 import Input from '@/app/ui/input';
 import Title from '@/app/ui/title';
+import { FC, useContext, useState } from 'react';
+import { CBTContext } from '../page';
 
-export default function Reframed() {
+interface ChildProps {
+	updateCbt: (value: { reframed: string }) => void;
+}
+
+export const Reframed: FC<ChildProps> = ({ updateCbt }) => {
+	const cbt = useContext(CBTContext);
+	const [inputValue, setInputValue] = useState(cbt.reframed);
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+		setInputValue(event.target.value);
+		updateCbt({ reframed: event.target.value });
+	};
+
 	return (
 		<div className="flex flex-col">
 			<Title text="4. How can you reframe this thought?" />
@@ -9,8 +23,10 @@ export default function Reframed() {
 				type="text"
 				id="evidence"
 				name="evidence"
+				value={inputValue}
 				placeholder="Enter your evidence here"
+				handleChange={(e) => handleChange(e)}
 			/>
 		</div>
 	);
-}
+};
