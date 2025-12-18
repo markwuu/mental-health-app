a7500e9ad4581f1a34bf1f0e05960acfffd10d3b
Mark W <markwuu@gmail.com>
2025-12-17 17:01:39 -0800

    added context to summary step

--- app/cognitive-behavioral-therapy/steps/summary.tsx
+++ app/cognitive-behavioral-therapy/steps/summary.tsx
@@ -1,13 +1,19 @@
 import Title from '@/app/ui/title';
+import { useContext } from 'react';
+import { CBTContext } from '../page';
 
 export default function Summary() {
+	const cbt = useContext(CBTContext);
+	const { thought } = cbt;
+
 	return (
 		<div className="flex flex-col">
 			<Title text="5. Summary" />
-			<h2>Automatic Thought:</h2>
-			<h2>Distortions:</h2>
-			<h2>Evidence:</h2>
-			<h2>Reframed Thought:</h2>
+			<h2 className="pt-7 font-extrabold text-lg">Automatic Thought:</h2>
+			<p> {thought} </p>
+			<h2 className="pt-7 font-extrabold text-lg">Distortions:</h2>
+			<h2 className="pt-7 font-extrabold text-lg">Evidence:</h2>
+			<h2 className="pt-7 font-extrabold text-lg">Reframed Thought:</h2>
 		</div>
 	);
 }
