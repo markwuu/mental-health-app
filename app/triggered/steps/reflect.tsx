54ecbe6d33d69530c06de7696dd7d036a57e84bb
Mark W <markwuu@gmail.com>
2025-12-23 11:23:02 -0800

    created reflect step component

---
+++ app/triggered/steps/reflect.tsx
@@ -0,0 +1,51 @@
+import Title from '@/app/ui/title';
+import { FC, useContext, useState } from 'react';
+import { TriggerContext } from '../page';
+
+interface ChildProps {
+	updateTrigger: (value: { reflect: number }) => void;
+}
+
+export const Reflect: FC<ChildProps> = ({ updateTrigger }) => {
+	const trigger = useContext(TriggerContext);
+	const [selectedValue, setSelectedValue] = useState(trigger.reflect);
+
+	const handleChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
+		const selectedNumber = parseInt(event.target.value, 10);
+		setSelectedValue(selectedNumber);
+		updateTrigger({ reflect: selectedNumber });
+	};
+
+	return (
+		<div className="flex flex-col">
+			<Title text="7. Reflect on the trigger" />
+			<p className="italic">Rate trigger intensity from 1-10</p>
+			<select
+				className="w-full appearance-none bg-white px-3 py-3 my-2 pr-10 text-base text-black border border-gray-300 rounded cursor-pointer max-w-2xs bg-no-repeat bg-right bg-size-[20px]"
+				style={{
+					backgroundImage: `url('data:image/svg+xml;utf8,<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 9L12 15L18 9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke="currentColor"/></svg>')`,
+					backgroundRepeat: 'no-repeat',
+					backgroundPosition: 'right 10px center',
+				}}
+				name="triggerLevel"
+				id="trigger-level-select"
+				value={selectedValue}
+				onChange={(e) => handleChange(e)}
+			>
+				<option disabled hidden>
+					Rate your trigger...
+				</option>
+				<option value={1}>1</option>
+				<option value={2}>2</option>
+				<option value={3}>3</option>
+				<option value={4}>4</option>
+				<option value={5}>5</option>
+				<option value={6}>6</option>
+				<option value={7}>7</option>
+				<option value={8}>8</option>
+				<option value={9}>9</option>
+				<option value={10}>10</option>
+			</select>
+		</div>
+	);
+};
