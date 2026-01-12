76099423dfcfe526d67b9f543e920291baaf5280
Mark W <markwuu@gmail.com>
2026-01-12 15:11:37 -0800

    created separate navlinks component

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,52 +1,14 @@
-'use client';
+import { fetchProfile } from './lib/data';
+import Navlinks from './navlinks';
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-
-export default function Navbar() {
-	const pathname = usePathname();
-	const currentTime = new Date();
-
-	const getGreetingText = () => {
-		const hour = currentTime.getHours();
-		if (hour >= 5 && hour < 12) {
-			return 'Good Morning, Em';
-		} else if (hour >= 12 && hour < 18) {
-			return 'Good Afternoon, Em';
-		} else {
-			return 'Good Evening, Em';
-		}
-	};
+export default async function Navbar() {
+	const profile = await fetchProfile();
+	const name = profile[0].name;
 
 	return (
 		<div className="flex-row w-7xl">
 			<div className="pt-8 px-10 text-xl">
-				<ul className="flex gap-8">
-					<li
-						className={`${
-							pathname === '/' ? 'underline underline-offset-6' : ''
-						}`}
-					>
-						<Link href="/">Home</Link>
-					</li>
-					<li
-						className={`${
-							pathname === '/cognitive-behavioral-therapy'
-								? 'underline underline-offset-6'
-								: ''
-						}`}
-					>
-						<Link href="/cognitive-behavioral-therapy">CBT</Link>
-					</li>
-					<li
-						className={`${
-							pathname === '/triggered' ? 'underline underline-offset-6' : ''
-						}`}
-					>
-						<Link href="/triggered">Triggered</Link>
-					</li>
-					<li className="ml-auto">{getGreetingText()}</li>
-				</ul>
+				<Navlinks name={name} />
 			</div>
 		</div>
 	);
---
+++ app/navlinks.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export default function Navlinks({ name }: { name: string }) {
+	console.log(`🚀 ~ Navlinks ~ name:`, name);
+	const pathname = usePathname();
+	const currentTime = new Date();
+
+	const getGreetingText = () => {
+		const hour = currentTime.getHours();
+		if (hour >= 5 && hour < 12) {
+			return `Morning, ${name}!`;
+		} else if (hour >= 12 && hour < 18) {
+			return `Afternoon, ${name}!`;
+		} else {
+			return `Evening, ${name}!`;
+		}
+	};
+
+	return (
+		<ul className="flex gap-8">
+			<li
+				className={`${pathname === '/' ? 'underline underline-offset-6' : ''}`}
+			>
+				<Link href="/">Home</Link>
+			</li>
+			<li
+				className={`${
+					pathname === '/cognitive-behavioral-therapy'
+						? 'underline underline-offset-6'
+						: ''
+				}`}
+			>
+				<Link href="/cognitive-behavioral-therapy">CBT</Link>
+			</li>
+			<li
+				className={`${
+					pathname === '/triggered' ? 'underline underline-offset-6' : ''
+				}`}
+			>
+				<Link href="/triggered">Triggered</Link>
+			</li>
+			<li className="ml-auto">{getGreetingText()}</li>
+		</ul>
+	);
+}
