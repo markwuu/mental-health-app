c0b4c45352eefb9b0b00f3daa6b6b385457e3d0d
Mark W <markwuu@gmail.com>
2026-01-12 22:08:02 -0800

    added media query for navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -6,7 +6,7 @@ export default async function Navbar() {
 	const name = profile[0].name;
 
 	return (
-		<div className="flex-row w-7xl">
+		<div className="flex-row w-3xl md:w-5xl">
 			<div className="pt-8 px-10 text-xl">
 				<Navlinks name={name} />
 			</div>
