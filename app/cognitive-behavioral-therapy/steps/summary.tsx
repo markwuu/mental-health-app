748045d6b499bd36779bcfc0835d1e898493acf5
Mark W <markwuu@gmail.com>
2026-01-12 20:09:50 -0800

    updated styles and displayed only checked distortion

--- app/cognitive-behavioral-therapy/steps/summary.tsx
+++ app/cognitive-behavioral-therapy/steps/summary.tsx
@@ -10,24 +10,26 @@ export default function Summary() {
 	return (
 		<div className="flex flex-col">
 			<Title text="Summary" />
-			<h2 className="pt-7 font-extrabold text-lg">1. Automatic Thought:</h2>
+			<h2 className="font-extrabold text-lg">1. Automatic Thought:</h2>
 			<p> {thought} </p>
-			<h2 className="pt-7 font-extrabold text-lg">2. Distortions:</h2>
+			<h2 className="pt-7 font-extrabold text-lg">2. Cognitive Distortions:</h2>
 			<div className="grid grid-cols-3 gap-2">
 				{distortions.map((distortion: { label: string; checked: boolean }) => {
-					return (
-						<div className="flex flex-row gap-3 py-1" key={distortion.label}>
-							<Input
-								type="checkbox"
-								id={distortion.label}
-								value={distortion.label}
-								name={distortion.label}
-								checked={distortion.checked}
-								readOnly={true}
-							/>
-							<label htmlFor={distortion.label}>{distortion.label}</label>
-						</div>
-					);
+					if (distortion.checked) {
+						return (
+							<div className="flex flex-row gap-3 py-1" key={distortion.label}>
+								<Input
+									type="checkbox"
+									id={distortion.label}
+									value={distortion.label}
+									name={distortion.label}
+									checked={distortion.checked}
+									readOnly={true}
+								/>
+								<label htmlFor={distortion.label}>{distortion.label}</label>
+							</div>
+						);
+					}
 				})}
 			</div>
 			<h2 className="pt-7 font-extrabold text-lg">3. Evidence:</h2>
