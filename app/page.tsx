322a2961cac35edd3864a73c65ae578976f7aba5
Mark W <markwuu@gmail.com>
2026-01-12 22:07:54 -0800

    added header from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import { fetchTriggers } from './lib/data';
+import Header from './ui/header';
 
 export default async function Home() {
 	const triggers = await fetchTriggers();
 	console.log(`🚀 ~ Home ~ triggers:`, triggers);
 
 	return (
-		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-			<main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-				hi
+		<div className="font-sans">
+			<main className="space-y-8 p-20 max-w-4xl w-187.5 mx-auto py-16 border-white border-2">
+				<Header text="Healing from Anxious Attachment" />
 			</main>
 		</div>
 	);
