386deb52a726ae155bb8643b3e32a99e8a36e11e
Mark W <markwuu@gmail.com>
2026-01-20 20:57:51 -0800

    decreased header font size

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,6 +1,6 @@
 export default function Header({ text }: { text: string }) {
 	return (
-		<h1 className="text-3xl text-center uppercase font-bold tracking-tight font-mono">
+		<h1 className="text-2xl text-center uppercase font-bold tracking-tight font-mono">
 			{text}
 		</h1>
 	);
