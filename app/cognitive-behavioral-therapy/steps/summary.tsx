b1a04c46a67f819cabed3f0c562e679152cd20b9
Mark W <markwuu@gmail.com>
2026-01-20 21:41:20 -0800

    created answer component and refactored

--- app/cognitive-behavioral-therapy/steps/summary.tsx
+++ app/cognitive-behavioral-therapy/steps/summary.tsx
@@ -2,6 +2,7 @@ import Title from '@/app/ui/title';
 import { useContext } from 'react';
 import { CBTContext } from '../page';
 import Input from '@/app/ui/input';
+import Answer from '@/app/ui/answer';
 
 export default function Summary() {
 	const cbt = useContext(CBTContext);
@@ -11,7 +12,7 @@ export default function Summary() {
 		<div className="flex flex-col">
 			<Title text="Summary" />
 			<h2 className="font-extrabold text-lg">1. Automatic Thought:</h2>
-			<p> {thought} </p>
+			<Answer text={thought} />
 			<h2 className="pt-7 font-extrabold text-lg">2. Cognitive Distortions:</h2>
 			<div className="grid grid-cols-3 gap-2">
 				{distortions.map((distortion: { label: string; checked: boolean }) => {
@@ -26,16 +27,18 @@ export default function Summary() {
 									checked={distortion.checked}
 									readOnly={true}
 								/>
-								<label htmlFor={distortion.label}>{distortion.label}</label>
+								<label htmlFor={distortion.label}>
+									<Answer text={distortion.label} />
+								</label>
 							</div>
 						);
 					}
 				})}
 			</div>
 			<h2 className="pt-7 font-extrabold text-lg">3. Evidence:</h2>
-			<p> {evidence} </p>
+			<Answer text={evidence} />
 			<h2 className="pt-7 font-extrabold text-lg">4. Reframed Thought:</h2>
-			<p> {reframed} </p>
+			<Answer text={reframed} />
 		</div>
 	);
 }
