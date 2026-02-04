c04cc39bbb1e326cdc458f6bfb057698a83dea1d
Mark W <markwuu@gmail.com>
2026-02-04 11:24:03 -0800

    added entries page

--- app/entries/page.tsx
+++ app/entries/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { createContext } from 'react';
 import { TriggerType } from '../lib/definitions';
 import NavigateButtons from '../components/navigateButtons';
 import Header from '../ui/header';
@@ -10,7 +9,7 @@ import Header from '../ui/header';
 export default function WorkingThroughATriggerPage() {
 	const router = useRouter();
 	const [currentPage, setCurrentPage] = useState('Trigger');
-	const [backButtonDisabled] = useState<boolean>(false);
+	const [backButtonDisabled] = useState<boolean>(true);
 	const [nextButtonDisabled, setNextButtonDisabled] = useState<boolean>(true);
 
 	const handleDisableButton = (value: TriggerType, type: string) => {
@@ -23,7 +22,14 @@ export default function WorkingThroughATriggerPage() {
 
 	return (
 		<div className="space-y-8 p-20 max-w-4xl w-187.5 mx-auto py-16">
-			<Header text="Cognitive Behavioral Therapy Entries" />
+			<Header text="Entries" />
+			<h1 className="text-center">Cognitive Behavioral Therapy Entries</h1>
+			<NavigateButtons
+				backButtonDisabled={backButtonDisabled}
+				nextButtonDisabled={nextButtonDisabled}
+				changePage={changePage}
+			/>
+			<h1 className="text-center">Triggered Entries</h1>
 			<NavigateButtons
 				backButtonDisabled={backButtonDisabled}
 				nextButtonDisabled={nextButtonDisabled}
