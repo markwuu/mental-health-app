ecf73e4856913a705a669ecf59ce2ce8f221a7ce
Mark W <markwuu@gmail.com>
2026-06-16 15:02:35 -0700

    refactored entry to new component

--- app/cognitive-behavioral-therapy/entries/[slug]/page.tsx
+++ app/cognitive-behavioral-therapy/entries/[slug]/page.tsx
@@ -1,8 +1,6 @@
 import { fetchCbtEntry } from '@/app/lib/data';
-import Answer from '@/app/ui/answer';
-import Input from '@/app/ui/input';
-import Title from '@/app/ui/title';
 import Link from 'next/link';
+import Entry from './entry';
 
 export default async function CognitiveBehavioralTherapyEntryPage({
 	params,
@@ -11,7 +9,8 @@ export default async function CognitiveBehavioralTherapyEntryPage({
 }) {
 	const { slug } = await params;
 	const entry = await fetchCbtEntry(slug);
-	const { thought, distortions, evidence, reframed } = entry[0];
+
+	if (!entry) return <div>loading...</div>;
 
 	return (
 		<div className='space-y-8 p-20 max-w-4xl w-187.5 mx-auto py-16'>
@@ -31,54 +30,11 @@ export default async function CognitiveBehavioralTherapyEntryPage({
 				>
 					Entries
 				</Link>
-				<span className='text-2xl text-center uppercase font-bold tracking-tight font-mono'>
-					{' > '}
-				</span>
-				<Link
-					href='/cognitive-behavioral-therapy/entries'
-					className='text-2xl text-center uppercase font-bold tracking-tight font-mono underline underline-offset-6 text-slate-500'
-				>
-					{slug}
-				</Link>
 			</div>
-			<div className='flex flex-col'>
-				<Title text='Summary' />
-				<h2 className='font-extrabold text-lg'>1. Automatic Thought:</h2>
-				<Answer text={thought} />
-				<h2 className='pt-7 font-extrabold text-lg'>
-					2. Cognitive Distortions:
-				</h2>
-				<div className='grid grid-cols-3 gap-2'>
-					{distortions.map(
-						(distortion: { label: string; checked: boolean }) => {
-							if (distortion.checked) {
-								return (
-									<div
-										className='flex flex-row gap-3 py-1 items-center'
-										key={distortion.label}
-									>
-										<Input
-											type='checkbox'
-											id={distortion.label}
-											value={distortion.label}
-											name={distortion.label}
-											checked={distortion.checked}
-											readOnly={true}
-										/>
-										<label htmlFor={distortion.label}>
-											<Answer text={distortion.label} />
-										</label>
-									</div>
-								);
-							}
-						},
-					)}
-				</div>
-				<h2 className='pt-7 font-extrabold text-lg'>3. Evidence:</h2>
-				<Answer text={evidence} />
-				<h2 className='pt-7 font-extrabold text-lg'>4. Reframed Thought:</h2>
-				<Answer text={reframed} />
+			<div className='text-center text-2xl underline underline-offset-10'>
+				Entry {slug}
 			</div>
+			<Entry data={entry[0]} />
 		</div>
 	);
 }
