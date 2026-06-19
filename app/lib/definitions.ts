cd06b0934ecbe7bc7d2b743c946b81034c33167a
Mark W <markwuu@gmail.com>
2026-06-19 02:39:51 -0700

    alphabetized distortion list

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -51,21 +51,21 @@ export interface CbtType {
 }
 
 export const distortionList = [
-	{ label: 'catastrophizing', checked: false },
-	{ label: 'should statements', checked: false },
-	{ label: 'magnification', checked: false },
-	{ label: 'minimization', checked: false },
-	{ label: 'emotional reasoning', checked: false },
-	{ label: 'mental filter', checked: false },
-	{ label: 'disqualify the positive', checked: false },
 	{ label: 'all-or-nothing thinking', checked: false },
-	{ label: 'personalization', checked: false },
 	{ label: 'blaming', checked: false },
-	{ label: 'mind reading', checked: false },
+	{ label: 'catastrophizing', checked: false },
+	{ label: 'disqualify the positive', checked: false },
+	{ label: 'emotional reasoning', checked: false },
 	{ label: 'fortune telling', checked: false },
-	{ label: 'labeling', checked: false },
-	{ label: 'overgeneralization', checked: false },
 	{ label: 'jumping to conclusions', checked: false },
+	{ label: 'labeling', checked: false },
+	{ label: 'magnification', checked: false },
+	{ label: 'mental filter', checked: false },
+	{ label: 'mind reading', checked: false },
+	{ label: 'minimization', checked: false },
+	{ label: 'overgeneralization', checked: false },
+	{ label: 'personalization', checked: false },
+	{ label: 'should statements', checked: false },
 ];
 
 export const emptyCBT = {
