9b25591f37e4c37a67435e56023b5953f90864c3
Mark W <markwuu@gmail.com>
2026-06-22 16:45:40 -0700

    added triggered page to breadcrumbs

--- app/components/breadcrumb.tsx
+++ app/components/breadcrumb.tsx
@@ -10,13 +10,17 @@ export default function Breadcrumb({ pageTitle }: { pageTitle: string }) {
 
 		return '';
 	};
+
+	const title =
+		pageTitle === 'cbt' ? 'Cognitive Behavior Therapy' : 'Triggered';
+
 	return (
-		<div className='text-center flex justify-between items-center'>
+		<div className='text-center flex justify-center items-center gap-3'>
 			<Link
 				href={href()}
 				className='bg-[#d02309] text-white text-2xl font-serif font-black italic tracking-[-0.06em] px-3 py-2 leading-none uppercase'
 			>
-				Cognitive Behavior Therapy
+				{title}
 			</Link>
 			<span className='text-xl text-center font-bold tracking-tight font-mono'>
 				{' > '}
