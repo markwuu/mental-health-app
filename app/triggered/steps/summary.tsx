6e1962bb35094c709ce7cde6fde662f09dd5cf7a
Mark W <markwuu@gmail.com>
2026-06-22 18:04:06 -0700

    added subtitle component and updated styles

--- app/triggered/steps/summary.tsx
+++ app/triggered/steps/summary.tsx
@@ -1,7 +1,7 @@
 import Title from '@/app/ui/title';
 import { useContext } from 'react';
 import { TriggerContext } from '../page';
-import Input from '@/app/ui/input';
+import Subtitle from '@/app/ui/subtitle';
 
 export const Summary = () => {
 	const trigger = useContext(TriggerContext);
@@ -36,62 +36,52 @@ export const Summary = () => {
 		return;
 	};
 
+	const highlightedAnswer =
+		'bg-[#d02309] text-white text-base font-serif font-black italic tracking-[-0.06em] px-3 py-2 leading-none uppercase';
+
 	return (
-		<div className="flex flex-col">
-			<Title text="Summary" />
-			<h2 className="font-extrabold text-lg">1. Acknowledge the trigger</h2>
-			<p className="italic">Rate trigger intensity from 1-10</p>
-			<p> {triggerLevel} </p>
-			<h2 className="pt-7 font-extrabold text-lg">2. Create Distance</h2>
-			<p className="italic">
-				Are you able to physically remove yourself from the trigger?
-			</p>
-			<p> {distance === true ? 'yes' : 'no'} </p>
-			<h2 className="pt-7 font-extrabold text-lg">3. Body Sensations</h2>
-			<p className="italic">Selected sensations happening in your body </p>
-			<div className="grid grid-cols-3 gap-2 py-1">
+		<div className='flex flex-col items-start'>
+			<Title text='Summary' />
+			<h2 className='font-extrabold text-lg'>1. Acknowledge the trigger</h2>
+			<Subtitle text='Rate trigger intensity from 1-10' />
+			<p className={highlightedAnswer}>{triggerLevel}</p>
+			<h2 className='pt-7 font-extrabold text-lg'>2. Create Distance</h2>
+			<Subtitle text='Are you able to physically remove yourself from the trigger?' />
+			<p className={highlightedAnswer}> {distance === true ? 'yes' : 'no'} </p>
+			<h2 className='pt-7 font-extrabold text-lg'>3. Body Sensations</h2>
+			<Subtitle text='Selected sensations happening in your body' />
+			<div className='grid grid-cols-3 gap-2 py-1'>
 				{sensations.map((sensation: { label: string; checked: boolean }) => {
 					if (sensation.checked) {
 						return (
 							<div
-								className="flex flex-row items-center gap-3 py-1"
+								className='flex flex-row items-center gap-3 py-1'
 								key={sensation.label}
 							>
-								<Input
-									type="checkbox"
-									id={sensation.label}
-									value={sensation.label}
-									name={sensation.label}
-									checked={sensation.checked}
-									readOnly={true}
-								/>
-								<label htmlFor={sensation.label}>{sensation.label}</label>
+								<label className={highlightedAnswer} htmlFor={sensation.label}>
+									{'\u2022 ' + sensation.label}
+								</label>
 							</div>
 						);
 					}
 				})}
 			</div>
-			<h2 className="pt-7 font-extrabold text-lg">4. Releasing Energy</h2>
-			<p className="italic">Selected activities to release negative energy </p>
-			<div className="grid grid-cols-3 gap-2 py-1">
+			<h2 className='pt-7 font-extrabold text-lg'>4. Releasing Energy</h2>
+			<Subtitle text='Selected activities to release negative energy' />
+			<div className='grid grid-cols-3 gap-2 py-1'>
 				{energyRelease.map(
 					(energyRelease: { label: string; checked: boolean }) => {
 						if (energyRelease.checked) {
 							return (
 								<div
-									className="flex flex-row items-center gap-3 py-1"
+									className='flex flex-row items-center gap-3 py-1'
 									key={energyRelease.label}
 								>
-									<Input
-										type="checkbox"
-										id={energyRelease.label}
-										value={energyRelease.label}
-										name={energyRelease.label}
-										checked={energyRelease.checked}
-										readOnly={true}
-									/>
-									<label htmlFor={energyRelease.label}>
-										{energyRelease.label}
+									<label
+										className={highlightedAnswer}
+										htmlFor={energyRelease.label}
+									>
+										{'\u2022 ' + energyRelease.label}
 									</label>
 								</div>
 							);
@@ -99,40 +89,35 @@ export const Summary = () => {
 					},
 				)}
 			</div>
-			<h2 className="pt-7 font-extrabold text-lg">5. Analyze the situation</h2>
-			<p className="italic">What am I experiencing?</p>
-			<p>{analyzeTrigger.experiencing}</p>
-			<p className="italic">What story am I telling myself?</p>
-			<p>{analyzeTrigger.story}</p>
-			<p className="italic">What exactly am I reacting to?</p>
-			<p>{analyzeTrigger.reactingTo}</p>
-			<p className="italic">Is my reaction appropriate?</p>
-			<p>{analyzeTrigger.appropriateReaction === true ? 'yes' : 'no'}</p>
-			<h2 className="pt-7 font-extrabold text-lg">
+			<h2 className='pt-7 font-extrabold text-lg'>5. Analyze the situation</h2>
+			<Subtitle text='What am I experiencing?' />
+			<p className={highlightedAnswer}>{analyzeTrigger.experiencing}</p>
+			<Subtitle text='What story am I telling myself?' />
+			<p className={highlightedAnswer}>{analyzeTrigger.story}</p>
+			<Subtitle text='What exactly am I reacting to?' />
+			<p className={highlightedAnswer}>{analyzeTrigger.reactingTo}</p>
+			<Subtitle text='Is my reaction appropriate?' />
+			<p className={highlightedAnswer}>
+				{analyzeTrigger.appropriateReaction === true ? 'yes' : 'no'}
+			</p>
+			<h2 className='pt-7 font-extrabold text-lg'>
 				6. Healing and finding your center
 			</h2>
-			<p className="italic">
-				Selected activities to bring yourself back to the present moment
-			</p>
-			<div className="grid grid-cols-3 gap-2 py-1">
+			<Subtitle text='Selected activities to bring yourself back to the present moment' />
+			<div className='grid grid-cols-3 gap-2 py-1'>
 				{trigger.healing.activities.map(
 					(healingActivity: { label: string; checked: boolean }) => {
 						if (healingActivity.checked) {
 							return (
 								<div
-									className="flex flex-row items-center gap-3 py-1"
+									className='flex flex-row items-center gap-3 py-1'
 									key={healingActivity.label}
 								>
-									<Input
-										type="checkbox"
-										id={healingActivity.label}
-										value={healingActivity.label}
-										name={healingActivity.label}
-										checked={healingActivity.checked}
-										readOnly={true}
-									/>
-									<label htmlFor={healingActivity.label}>
-										{healingActivity.label}
+									<label
+										className={highlightedAnswer}
+										htmlFor={healingActivity.label}
+									>
+										{'\u2022 ' + healingActivity.label}
 									</label>
 								</div>
 							);
@@ -140,15 +125,15 @@ export const Summary = () => {
 					},
 				)}
 			</div>
-			<p className="italic">Whats a healthy next step I can give myself?</p>
-			<p>{healing.giveMyself}</p>
-			<p className="italic">Whats a healthy next step I can give my partner?</p>
-			<p>{healing.givePartner}</p>
-			<h2 className="pt-7 font-extrabold text-lg">
+			<Subtitle text='Whats a healthy next step I can give myself?' />
+			<p className={highlightedAnswer}>{healing.giveMyself}</p>
+			<Subtitle text='Whats a healthy next step I can give my partner?' />
+			<p className={highlightedAnswer}>{healing.givePartner}</p>
+			<h2 className='pt-7 font-extrabold text-lg'>
 				7. Reflecting on the trigger
 			</h2>
-			<p className="italic">Rate trigger intensity from 1-10</p>
-			<p>{finalPercent()}</p>
+			<Subtitle text='Rate trigger intensity from 1-10' />
+			<p className={highlightedAnswer}>{finalPercent()}</p>
 		</div>
 	);
 };
