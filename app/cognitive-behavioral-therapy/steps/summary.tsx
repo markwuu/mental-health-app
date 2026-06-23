6b759db54ecfd439931229e47ccf3498ab1c46e1
Mark W <markwuu@gmail.com>
2026-06-23 14:02:51 -0700

    updated styles on summary cbt component

--- app/cognitive-behavioral-therapy/steps/summary.tsx
+++ app/cognitive-behavioral-therapy/steps/summary.tsx
@@ -9,21 +9,21 @@ export default function Summary() {
 	const { thought, distortions, evidence, reframed } = cbt;
 
 	return (
-		<div className="flex flex-col">
-			<Title text="Summary" />
-			<h2 className="font-extrabold text-lg">1. Automatic Thought:</h2>
+		<div className='flex flex-col items-start'>
+			<Title text='Summary' />
+			<h2 className='font-extrabold text-lg'>1. Automatic Thought:</h2>
 			<Answer text={thought} />
-			<h2 className="pt-7 font-extrabold text-lg">2. Cognitive Distortions:</h2>
-			<div className="grid grid-cols-3 gap-2">
+			<h2 className='pt-7 font-extrabold text-lg'>2. Cognitive Distortions:</h2>
+			<div className='grid grid-cols-3 gap-2'>
 				{distortions.map((distortion: { label: string; checked: boolean }) => {
 					if (distortion.checked) {
 						return (
 							<div
-								className="flex flex-row gap-3 py-1 items-center"
+								className='flex flex-row gap-3 py-1 items-center'
 								key={distortion.label}
 							>
 								<Input
-									type="checkbox"
+									type='checkbox'
 									id={distortion.label}
 									value={distortion.label}
 									name={distortion.label}
@@ -38,9 +38,9 @@ export default function Summary() {
 					}
 				})}
 			</div>
-			<h2 className="pt-7 font-extrabold text-lg">3. Evidence:</h2>
+			<h2 className='pt-7 font-extrabold text-lg'>3. Evidence:</h2>
 			<Answer text={evidence} />
-			<h2 className="pt-7 font-extrabold text-lg">4. Reframed Thought:</h2>
+			<h2 className='pt-7 font-extrabold text-lg'>4. Reframed Thought:</h2>
 			<Answer text={reframed} />
 		</div>
 	);
